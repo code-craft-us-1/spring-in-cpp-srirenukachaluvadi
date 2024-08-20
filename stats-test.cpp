c99a122071753221cdfcbf316052f265df49fa2b
srirenukachaluvadi <nagavenkatasrirenuka.chaluvadi@philips.com>
2024-08-20 15:52:25 +0530

    changes

--- stats-test.cpp
+++ stats-test.cpp
@@ -18,7 +18,7 @@ TEST(Statistics, AverageNaNForEmpty) {
     
     //Design the gtest EXPECT statement here.
     //Use http://www.cplusplus.com/reference/cmath/isnan/
-	EXPECT_TRUE(std::isNan(computedStats.average)==1);
-	EXPECT_TRUE(std::isNan(computedStats.max)==1);
-	EXPECT_TRUE(std::isNan(computedStats.min)==1);
+	EXPECT_TRUE(isnan(computedStats.average)==1);
+	EXPECT_TRUE(isnan(computedStats.max)==1);
+	EXPECT_TRUE(isnan(computedStats.min)==1);
 }
