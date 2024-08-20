cff2f85d3101f536e3bd2f3b79cea6129294d6b2
srirenukachaluvadi <nagavenkatasrirenuka.chaluvadi@philips.com>
2024-08-20 20:26:55 +0530

    changes

--- stats.cpp
+++ stats.cpp
@@ -1,9 +1,9 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& a) {
+ Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& a) {
     //Implement statistics here
-	Stats k;
-	k.average=accumulate( a.begin(), a.end(), 0.0 )/a.size();
+	Statistics::Stats k;
+	k.average=std::accumulate( a.begin(), a.end(), 0.0 )/a.size();
 	k.max=*std::max_element(a.begin(), a.end());
 	k.min=*std::min_element(a.begin(), a.end());
 	return k;
--- stats.h
+++ stats.h
@@ -2,10 +2,10 @@
 
 namespace Statistics {
     // define the Stats structure here. See the tests to infer its properties
-    typedef struct Stats{
-	float average;
-	float max;
-	float min;
+    struct Stats{
+	float average=0.0f;
+	float max=0.0f;
+	float min=0.0f;
 	};
     Stats ComputeStatistics(const std::vector<float>& );
 	
