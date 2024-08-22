0416b04e0dafa3da6ebb06785a0bc89b9c70d40f
srirenukachaluvadi <nagavenkatasrirenuka.chaluvadi@philips.com>
2024-08-22 16:37:31 +0530

    changes

--- stats.cpp
+++ stats.cpp
@@ -6,8 +6,18 @@
  Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& a) {
     //Implement statistics here
 	Statistics::Stats k;
-	k.average=std::accumulate( a.begin(), a.end(), 0.0 )/a.size();
-	k.max=*std::max_element(a.begin(), a.end());
-	k.min=*std::min_element(a.begin(), a.end());
+	k.average = std::accumulate(a.begin(), a.end(), 0.0) / a.size();
+
+	auto const max = std::max_element(a.begin(), a.end());
+	auto const min = std::min_element(a.begin(), a.end());
+	if(max==a.end())
+	{
+		k.max =nan("0x12345") ;
+	}
+	if(min==a.end())
+	{
+		k.min = nan("0x12345");;
+	}
+		
 	return k;
 }
