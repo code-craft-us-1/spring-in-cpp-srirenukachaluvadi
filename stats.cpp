e6271bdf411acfc2871c766d753396479aeb6f27
srirenukachaluvadi <nagavenkatasrirenuka.chaluvadi@philips.com>
2024-08-22 16:47:20 +0530

    changes

--- stats.cpp
+++ stats.cpp
@@ -2,22 +2,23 @@
 #include <algorithm>
 #include <numeric>
 #include <vector>
+#include <math.h>
+#include<corecrt_math.h>
 
  Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& a) {
     //Implement statistics here
 	Statistics::Stats k;
-	k.average = std::accumulate(a.begin(), a.end(), 0.0) / a.size();
+	k.average = static_cast<float>(std::accumulate(a.begin(), a.end(), 0.0) / a.size());
 
 	auto const max = std::max_element(a.begin(), a.end());
 	auto const min = std::min_element(a.begin(), a.end());
 	if(max==a.end())
 	{
-		k.max =nan("0x12345") ;
+		k.max =static_cast<float>(nan("0x12345")) ;
 	}
 	if(min==a.end())
 	{
-		k.min = nan("0x12345");;
+		k.min = static_cast<float>(nan("0x12345"));
 	}
-		
 	return k;
 }
