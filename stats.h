d8afece18dedb5d4798a4ba4ff065c4f60a7d6a7
srirenukachaluvadi <nagavenkatasrirenuka.chaluvadi@philips.com>
2024-08-22 16:57:35 +0530

    changes

--- stats.h
+++ stats.h
@@ -3,9 +3,9 @@
 namespace Statistics {
     // define the Stats structure here. See the tests to infer its properties
     struct Stats{
-	float average=0.0f;
-	float max=0.0f;
-	float min=0.0f;
+	float average;
+	float max;
+	float min;
 	};
     Stats ComputeStatistics(const std::vector<float>& );
 	
